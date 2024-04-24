f50382764a9a18ca8213d0c8a1e5d694ce4ce9a6
tct123 <42028373+tct123@users.noreply.github.com>
2024-04-24 15:21:13 +0200

    Initial commit

---
+++ myemail/__init__.py
@@ -0,0 +1,1 @@
+from myemail.myemail import send
---
+++ myemail/myemail.py
@@ -0,0 +1,17 @@
+def send(msg_content,from_email, to_email,subject):
+    msg_content = msg_content
+    message = EmailMessage()
+    message["From"] = from_email
+    message["To"] = to_email
+    message["Subject"] = subject
+    message.set_content(msg_content)
+    try:
+        server = smtplib.SMTP(smtpserver, sslport)
+        server.starttls()
+        server.login(me, password)
+        server.send_message(message)
+        print("E-Mail erfolgreich gesendet!")
+    except Exception as e:
+        print(f"Fehler beim Senden der E-Mail: {e}")
+    finally:
+        server.quit()
